8c96df45a9ed6240b7f173835f122c8ae0886f86
Jorge Vidal Orozco <jvidal@novopayment.com>
2023-06-15 14:39:55 -0500

    Update LoginViewModel.kt

--- app/src/main/java/com/celeste/composeinstagram/login/ui/LoginViewModel.kt
+++ app/src/main/java/com/celeste/composeinstagram/login/ui/LoginViewModel.kt
@@ -1,11 +1,19 @@
 package com.celeste.composeinstagram.login.ui
 
+import android.util.Log
 import android.util.Patterns
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.celeste.composeinstagram.login.domain.LoginUseCase
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import kotlin.coroutines.coroutineContext
 
 class LoginViewModel: ViewModel() {
+
+    private val loginUseCase = LoginUseCase()
 
     private val _email = MutableLiveData<String>()
     val email: LiveData<String> = _email
@@ -16,11 +24,32 @@ class LoginViewModel: ViewModel() {
     private val _isLoginEnabled = MutableLiveData<Boolean>()
     val isLoginEnabled: LiveData<Boolean> = _isLoginEnabled
 
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
+
     fun onLoginChanged(email: String, password: String) {
         _email.value = email
         _password.value = password
 
         _isLoginEnabled.value = enableLogin(email, password)
+    }
+
+    fun onLoginClick() {
+        _isLoading.value = true
+
+        viewModelScope.launch {
+            val result = loginUseCase.invoke(
+                user = email.value.orEmpty(),
+                password = password.value.orEmpty()
+            )
+
+            if(result) {
+                //Navegar a la siguiente pantalla
+                Log.i("","Result Ok")
+            }
+
+            _isLoading.value = false
+        }
     }
 
     private fun enableLogin(email: String, password: String): Boolean {
